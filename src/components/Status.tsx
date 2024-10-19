b66635a0a839cf280ea20ecb33467b4165836c1a
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-19 19:22:13 +0600

    status type props

---
+++ src/components/Status.tsx
@@ -0,0 +1,23 @@
+type StatusProps = {
+    status: 'loading' | 'success' | 'error'
+}
+
+const Status = (props: StatusProps) => {
+    let message;
+    if(props.status === "loading"){
+        message = "Loading..."
+    }
+    else if(props.status === "success"){
+        message = "Data fetched successfully."
+    }
+    else if(props.status === "error"){
+        message = "Error! failed to fetch."
+    }
+    return (
+        <div>
+            <h2>{message}</h2>
+        </div>
+    );
+};
+
+export default Status;
