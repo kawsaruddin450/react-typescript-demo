a9725365633ed0b6b75ef4cf8ad8e499cbbfdcae
kawsar uddin patoary <kawsaruddin450@gmail.com>
2024-10-19 20:07:18 +0600

    onchange event type props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Button handleClick={()=> {
         console.log("Button clicked!")
       }}></Button>
+      <Input value='Kawsar' handleChange={(event) => console.log(event)}></Input>
     </div>
   );
 }
